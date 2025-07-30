1e7b2d96b49b40a870d8a68f2575f71e2f8febb4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-29 20:42:18 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,10 @@
 pip install mkdocs-material
 mkdocs serve
+
+# Clone the repo
+git clone https://github.com/Web4application/web-4.git
+cd web-4
+
+# Build and launch services
+./build-nacos.sh
+docker-compose up
